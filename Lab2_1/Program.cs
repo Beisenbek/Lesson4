61ae7aac74cc6fc266678ff69191864bb73a0ae3
beysenbek <beysenbek@gmail.com>
2015-01-31 21:06:34 +0400

    fix

---
+++ Lab2_1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(Environment.SystemDirectory);
+            //ShowDirectory(dir);
+            FSW();
+        }
+
+
+        static void ShowDirectory(DirectoryInfo dirInfo)
+        {
+            var x = dirInfo.GetFiles();
+
+            for (int i = 0; i < x.Length; ++i)
+            {
+                Console.WriteLine(x[i].FullName);
+            }
+        }
+        static FileSystemWatcher fsw = new FileSystemWatcher(@"C:\Users\bsnbk2\Desktop");
+            
+        static void FSW()
+        {
+            fsw.EnableRaisingEvents = true;
+            fsw.NotifyFilter = NotifyFilters.Size|NotifyFilters.FileName;
+            fsw.Changed += fsw_Changed;
+            fsw.WaitForChanged(WatcherChangeTypes.Changed);
+        }
+
+        static void fsw_Changed(object sender, FileSystemEventArgs e)
+        {
+            try
+            {
+                Console.WriteLine("Smthing changed!");
+            }
+            finally
+            {
+                fsw.EnableRaisingEvents = false;
+            }
+        }
+    }
+}
