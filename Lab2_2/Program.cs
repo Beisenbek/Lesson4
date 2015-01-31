3daf4d40d5cd387234b2c27377512ea5b48d021f
beysenbek <beysenbek@gmail.com>
2015-01-31 21:30:32 +0400

    fix4

---
+++ Lab2_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part2();  
+        }
+
+        static void Patr1()
+        {
+            StreamWriter sw = new StreamWriter(@"C:\Users\bsnbk2\Desktop\new.txt", true);
+
+
+            sw.WriteLine("hello world!");
+
+            sw.Close();
+
+        }
+
+        static void Part2()
+        {
+            StreamReader sr = File.OpenText(@"C:\Users\bsnbk2\Desktop\new.txt");
+
+            try
+            {
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                sr.Close();
+            }
+        }
+    }
+}
